5dfb8d4bc9187bf6f453fea442b5e6eb5cfe9462
kaczmarek.pi <kaczmarek.pi@totalizator.pl>
2023-11-01 11:51:16 +0100

    Add styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import './app.scss';
 import './index.css';
 
 const App = () => {
   return (
-    <>
-      <p>Boilerplate</p>
-    </>
+    <div className='container'>
+      <div className='card'>
+        <div className='wrapper'>
+          <img src='/background.png' className='card__background venom' />
+        </div>
+        <img src='/title.png' className='card__title' />
+        <img src='venom.png' className='card__front venom' />
+      </div>
+      <div className='card'>
+        <div className='wrapper'>
+          <img src='/background.png' className='card__background venom' />
+        </div>
+        <img src='/title.png' className='card__title' />
+        <img src='venom.png' className='card__front venom' />
+      </div>
+    </div>
   );
 };
 
